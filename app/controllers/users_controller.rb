b56a026de402c5f1973cd62969b531417c283823
timfesta <timothyfesta@yahoo.com>
2015-11-03 05:06:12 -0800

    added editing feature

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,33 @@
+class UsersController < ApplicationController
+
+	def show
+		@user = User.find(params[:id])		
+	end
+
+	def new
+		@user = User.new
+	end
+
+	def create
+		@user = User.new(user_params)
+		if @user.save
+			redirect_to @user
+		else
+			render 'new'
+		end
+	end
+
+	def edit
+		@user = User.find(params[:id])		
+	end
+
+	def update
+		
+	end
+
+	private
+	def user_params
+		params.require(:user).permit(:name, :email)
+	end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,11 @@
 Rails.application.routes.draw do
+  get 'signup' => 'users#new'
+
+  get 'profile' => 'users#show'
   
+  resources :users
+
   root 'statics#index'
-  
+
 end
+
